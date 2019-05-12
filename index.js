49ea7a604f1234d6f9ed45881c1ec4f55720f9bf
Nick Angelou <nick.angelou@sectify.com>
2019-05-11 20:17:58 -0500

    feat: working decryption!

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const Crypt = require('./src/adapters/Crypt')
 const schemeType = Crypt.schemeType({type: 'BFV'})
 Crypt.initializeLow({schemeType})
 Crypt.genKeys()
-console.log(Crypt.secretKey())
+let encrypted = Crypt.encrypt({value: 2147483647})
+let decrypted = Crypt.decrypt({cipherText: encrypted})
+
+console.log('decrypted:', decrypted)
