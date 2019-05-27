99d383fa5319e4f8a0c65120d886eda268315c2a
Nick Angelou <angelou.nick@gmail.com>
2019-05-26 20:40:18 -0500

    feat: added README.md, changed bit level security

--- src/test/generate-relin-keys.test.js
+++ src/test/generate-relin-keys.test.js
@@ -4,7 +4,7 @@ describe.skip('Generate RelinKeys', () => {
     test('low', async () => {
       const { HCrypt } = process.env.NODE_ENV === 'development'? require('../index.js') : require('../../dist/hcrypt.node.js')
       const Crypt = await HCrypt
-      const parms = Crypt.createParams({security: 'low'})
+      const parms = Crypt.createParams({computationLevel: 'low'})
       Crypt.initialize({...parms, schemeType: 'BFV'})
       const spy = jest.spyOn(Crypt, 'genRelinKeys')
       Crypt.genKeys()
@@ -17,7 +17,7 @@ describe.skip('Generate RelinKeys', () => {
     test('low', async () => {
       const { HCrypt } = process.env.NODE_ENV === 'development'? require('../index.js') : require('../../dist/hcrypt.node.js')
       const Crypt = await HCrypt
-      const parms = Crypt.createParams({security: 'low'})
+      const parms = Crypt.createParams({computationLevel: 'low'})
       Crypt.initialize({...parms, schemeType: 'CKKS'})
       const spy = jest.spyOn(Crypt, 'genRelinKeys')
       Crypt.genKeys()
