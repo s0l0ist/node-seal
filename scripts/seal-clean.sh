134f98ee12c7cee72b69de03b5dd22a268a5fd64
Nick Angelou <nick.angelou@sectify.com>
2019-06-04 14:11:20 -0500

    fix: clean script working on ubuntu

--- scripts/seal-clean.sh
+++ scripts/seal-clean.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf ./src/bin/* && cd ./submodules/SEAL/native rm -rf ./bin/* && rm -rf ./lib/*  && rm -rf ./src/CMakeFiles && rm -rf ./src/CMakeCache.txt && cd ../../../
+rm -rf ./src/bin/* && cd ./submodules/SEAL/native && rm -rf ./bin/* && rm -rf ./lib/*  && rm -rf ./src/CMakeFiles && rm -rf ./src/CMakeCache.txt && cd ../../../
