81b9e321672a6c4ac5eb27e1dc326fea9fca484a
Nick Angelou <nick.angelou@sectify.com>
2019-09-29 18:47:57 -0500

    feat: added expandModChain and manual overrides for polyModulusDegree and scale.

--- scripts/seal-build.sh
+++ scripts/seal-build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-source ./submodules/emsdk/emsdk_env.sh && cd ./submodules/SEAL/native/lib/ && em++ -Wall -Os --bind -o seal.js libseal.so -s DISABLE_EXCEPTION_CATCHING=0 -s ALLOW_MEMORY_GROWTH=1 -s EXPORT_ES6=1 -s MODULARIZE=1 --closure 1 -s "BINARYEN_TRAP_MODE='clamp'" -s SINGLE_FILE=1 && cp seal* ../../../../src/bin/ && cd ../../../
+# Ensure we are building with no exception handling. This reduces size by ~30%.
+source ./submodules/emsdk/emsdk_env.sh && cd ./submodules/SEAL/native/lib/ && em++ -Wall -Os --bind -o seal.js libseal.so -s DISABLE_EXCEPTION_CATCHING=1 -s NODEJS_CATCH_EXIT=0 -s ALLOW_MEMORY_GROWTH=1 -s EXPORT_ES6=1 -s MODULARIZE=1 --closure 1 -s "BINARYEN_TRAP_MODE='clamp'" -s SINGLE_FILE=1 && cp seal* ../../../../src/bin/ && cd ../../../
+
+# Build with exception handling. Size of WebAssembly is larger.
+#source ./submodules/emsdk/emsdk_env.sh && cd ./submodules/SEAL/native/lib/ && em++ -Wall -Os --bind -o seal.js libseal.so -s DISABLE_EXCEPTION_CATCHING=0 -s NODEJS_CATCH_EXIT=1 -s ALLOW_MEMORY_GROWTH=1 -s EXPORT_ES6=1 -s MODULARIZE=1 --closure 1 -s "BINARYEN_TRAP_MODE='clamp'" -s SINGLE_FILE=1 && cp seal* ../../../../src/bin/ && cd ../../../
