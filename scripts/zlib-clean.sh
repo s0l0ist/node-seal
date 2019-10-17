651e860c1773b8db8521b8ba545fe4da1ae5a2d5
Nick Angelou <nick.angelou@sectify.com>
2019-10-17 17:44:36 -0500

    chore: built latest library, removed zlib for future release

--- scripts/zlib-clean.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cd ./submodules/zlib && rm -rf ./CMakeFiles ./CMakeCache.txt ./CTestTestfile.cmake ./cmake_install.cmake && cd ../../
--- scripts/zlib-make.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cd ./submodules/zlib && make -j`python -c 'import multiprocessing as mp; print(mp.cpu_count())'` && cd ../../
