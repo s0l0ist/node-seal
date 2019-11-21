e80855583c8464ef0f776d58404ead96bca41692
Nick Angelou <angelou.nick@gmail.com>
2019-11-21 11:19:00 -0600

    feat: upgraded emscripten backend to LLVM

--- scripts/em-init.sh
+++ scripts/em-init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-# as of 11/19/2019, the incoming release works, but not sdk-1.39.2.
-cd submodules/emsdk/ && ./emsdk install sdk-incoming-64bit && ./emsdk activate sdk-incoming-64bit && cd ../../
+# Sets up llvm instead of fastcomp.
+cd submodules/emsdk/ && ./emsdk install latest && ./emsdk activate latest && cd ../../
+
+# Sets up fastcomp (previous bacckend)
+#cd submodules/emsdk/ && ./emsdk install latest-fastcomp && ./emsdk activate latest-fastcomp && cd ../../
