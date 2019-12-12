f1d6d566d2ed5bfc311817e0a571dabe944336e3
Nick Angelou <nick@morfix.io>
2019-12-11 18:08:19 -0600

    feat: added generated test files

---
+++ src/test/galoiskey/galoiskey-scheme_BFV-poly_8192-sec_256.test.js
@@ -0,0 +1,58 @@
+describe('galoiskey on BFV', () => {
+  describe('polyModulusDegree 8192', () => {
+    test('256-bit security', async () => {
+      const { Seal } = require('../../index.js')
+      const Morfix = await Seal
+      const parms = Morfix.EncryptionParameters({
+        schemeType: Morfix.SchemeType.BFV
+      })
+
+      parms.setPolyModulusDegree({
+        polyModulusDegree: 8192
+      })
+        
+      // Create a suitable vector of CoeffModulus primes
+      parms.setCoeffModulus({
+        coeffModulus: Morfix.CoeffModulus.Create({
+          polyModulusDegree: 8192,
+          bitSizes: Morfix.Vector({array: new Int32Array([39,39,40]) }),
+          securityLevel: Morfix.SecurityLevel.tc256
+        })
+      })
+      
+      // Set the PlainModulus to a prime of bitSize 20.
+      parms.setPlainModulus({
+        plainModulus: Morfix.PlainModulus.Batching({
+          polyModulusDegree: 8192,
+          bitSize: 20
+        })
+      })
+      
+      const context = Morfix.Context({
+        encryptionParams: parms,
+        expandModChain: true,
+        securityLevel: Morfix.SecurityLevel.tc256
+      })
+
+      expect(context.parametersSet()).toBe(true)
+
+      const keyGenerator = Morfix.KeyGenerator({
+        context: context
+      })
+      
+      expect(context.parametersSet()).toBe(true)
+
+      const spyGenGaloisKeys = jest.spyOn(keyGenerator, 'genGaloisKeys')
+      const galoisKeys = keyGenerator.genGaloisKeys()
+      expect(spyGenGaloisKeys).toHaveBeenCalled()
+      
+      const spySaveGaloisKeys = jest.spyOn(galoisKeys, 'save')
+      const base64 = galoisKeys.save()
+      expect(spySaveGaloisKeys).toHaveBeenCalled()
+      
+      const spyLoadGaloisKeys = jest.spyOn(galoisKeys, 'load')
+      galoisKeys.load({context, encoded: base64})
+      expect(spyLoadGaloisKeys).toHaveBeenCalled()
+    })
+  })
+})
