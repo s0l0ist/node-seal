207b7c4203e384dbdb97db3311a84c14a411698f
Nick Angelou <nick@morfix.io>
2020-02-25 22:08:27 -0600

    feat: added optional instance parameter to the constructors of keys

--- src/components/public-key.js
+++ src/components/public-key.js
@@ -1,9 +1,14 @@
-export const PublicKey = library => (Exception, ComprModeType) => () => {
-  let _instance = null
-  try {
-    _instance = new library.PublicKey()
-  } catch (e) {
-    throw Exception.safe(e)
+export const PublicKey = library => (Exception, ComprModeType) => (
+  instance = null
+) => {
+  let _instance = instance
+
+  if (!instance) {
+    try {
+      _instance = new library.PublicKey()
+    } catch (e) {
+      throw Exception.safe(e)
+    }
   }
 
   /**
