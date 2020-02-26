79d1d63140a9d520240606ff739004bab5cb2482
Nick Angelou <nick@morfix.io>
2020-02-26 12:29:16 -0600

    chore: refactored library initialization

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,4 @@
-import source from '../bin/seal.js'
-import sourceWasm from '../bin/seal'
+import source from '../bin/seal'
 import { Library } from './library'
 import { SEAL } from './Seal'
 import * as utils from './utils'
@@ -11,10 +10,11 @@ export const Seal = (async () => {
   /*
    * First, we initialize the library which loads from a WASM file/base64 string.
    * This file is loaded asynchronously and therefore we must wait
-   * until it has fully initialized.
+   * until it has fully initialized. The promise breaks when attempting to
+   * resolve the module...
    */
-  const lib = Library()
-  await lib.initialize({ source, sourceWasm })
+  const lib = await Library(source)
+  await lib.initialize()
 
   // Add a component
   const addToComponent = lib => (acc, [key, val]) => {
--- src/lib/library.js
+++ src/lib/library.js
@@ -1,40 +1,22 @@
-import pathlib from 'path'
-
-export const Library = () => {
-  let _module = null
-  let _timeout = null
+export const Library = source => {
+  let _instance = null
 
   return {
     get instance() {
-      return _module
+      return _instance
     },
 
-    initialize: async ({ source, sourceWasm }) => {
+    initialize: async () => {
       return await new Promise((resolve, reject) => {
-        // If we tell emscripten to build both js and wasm, this code is needed
-        // Right now, we use single file js where the wasm is a base64 string so
-        // this code block doesn't execute.
-        const module = source({
-          locateFile(path) {
-            // for jest
-            if (process.env.NODE_ENV !== 'production') {
-              return pathlib.resolve(__dirname, '../bin', path)
-            }
-
-            if (path.endsWith('.wasm')) {
-              return sourceWasm
-            }
-            return path
-          }
-        })
-
-        _timeout = setTimeout(() => {
+        const timeout = setTimeout(() => {
           reject(new Error('Could not initialize library in time!'))
         }, 10000)
 
+        const module = source()
+
         module.onRuntimeInitialized = () => {
-          clearTimeout(_timeout)
-          _module = module
+          clearTimeout(timeout)
+          _instance = module
           resolve()
         }
       })
