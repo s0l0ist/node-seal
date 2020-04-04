29475b01035fb6f94ecc7b53055b8ce44159cd34
Nick Angelou <angelou.nick@gmail.com>
2020-04-04 15:14:54 -0500

    chore: made the scripts easier to read

--- scripts/zlib-make.sh
+++ scripts/zlib-make.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-source ./submodules/emsdk/emsdk_env.sh && cd ./submodules/zlib && emmake make -j$(python -c 'import multiprocessing as mp; print(mp.cpu_count())') && cd ../../../
+source ./submodules/emsdk/emsdk_env.sh \
+&& cd ./submodules/zlib \
+&& emmake make -j$(python -c 'import multiprocessing as mp; print(mp.cpu_count())') \
+&& cd ../../../
