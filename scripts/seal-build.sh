d0c63d50d7cfde630bd1a44564723ac70cc18ec3
Nick Angelou <angelou.nick@gmail.com>
2020-04-04 14:48:50 -0500

    fix: build options for emscripten

--- scripts/seal-build.sh
+++ scripts/seal-build.sh
@@ -3,21 +3,19 @@
 source ./submodules/emsdk/emsdk_env.sh && cd ./submodules/SEAL/native/lib/ && \
 em++ -Wall -O3 --bind -o seal.js libseal-* ../../../zlib/libz.* \
 -s WASM=1 \
--s INITIAL_MEMORY=268435456 \
 -s ALLOW_MEMORY_GROWTH=1 \
 -s EXPORT_ES6=1 \
 -s MODULARIZE=1 \
 -s USE_ES6_IMPORT_META=0 \
 -s SINGLE_FILE=1 \
 --closure 1 \
---llvm-lto 1 \
 && cp seal* ../../../../src/bin/ && cd ../../../
 
 # Pure JS build
 #source ./submodules/emsdk/emsdk_env.sh && cd ./submodules/SEAL/native/lib/ && \
 #em++ -Wall -O2 --bind -o seal.js libseal-* ../../../zlib/libz.* \
 #-s WASM=0 \
-#-s TOTAL_MEMORY=1024MB \
+#-s INITIAL_MEMORY=268435456 \
 #-s ALLOW_MEMORY_GROWTH=0 \
 #-s EXPORT_ES6=1 \
 #-s MODULARIZE=1 \
