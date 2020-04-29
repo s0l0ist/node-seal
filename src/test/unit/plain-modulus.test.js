2bc106dc846657e770150cd27e9ebd1f29defac5
Nick Angelou <angelou.nick@gmail.com>
2020-04-28 19:43:04 -0500

    wip: fixed a few tests

--- src/test/unit/plain-modulus.test.js
+++ src/test/unit/plain-modulus.test.js
@@ -24,35 +24,35 @@ describe('PlainModulus', () => {
     expect(PlainModulusObject).toHaveProperty('BatchingVector')
   })
 
-  test('It should a return a SmallModulus', () => {
+  test('It should a return a Modulus', () => {
     const spyOn = jest.spyOn(PlainModulusObject, 'Batching')
-    const smallModulus = PlainModulusObject.Batching(4096, 20)
+    const modulus = PlainModulusObject.Batching(4096, 20)
     expect(spyOn).toHaveBeenCalledWith(4096, 20)
-    expect(smallModulus).toBeDefined()
-    expect(typeof smallModulus.constructor).toBe('function')
-    expect(smallModulus).toBeInstanceOf(Object)
-    expect(smallModulus.constructor).toBe(Object)
-    expect(smallModulus.constructor.name).toBe('Object')
-    expect(smallModulus.instance.constructor.name).toBe('SmallModulus')
+    expect(modulus).toBeDefined()
+    expect(typeof modulus.constructor).toBe('function')
+    expect(modulus).toBeInstanceOf(Object)
+    expect(modulus.constructor).toBe(Object)
+    expect(modulus.constructor.name).toBe('Object')
+    expect(modulus.instance.constructor.name).toBe('Modulus')
   })
-  test('It should fail to return a SmallModulus', () => {
+  test('It should fail to return a Modulus', () => {
     const spyOn = jest.spyOn(PlainModulusObject, 'Batching')
     expect(() => PlainModulusObject.Batching(4095, 20)).toThrow()
     expect(spyOn).toHaveBeenCalledWith(4095, 20)
   })
-  test('It should a create a Vector of SmallModulus', () => {
+  test('It should a create a Vector of Modulus', () => {
     const spyOn = jest.spyOn(PlainModulusObject, 'BatchingVector')
-    const vectSmallModulus = PlainModulusObject.BatchingVector(
+    const vectModulus = PlainModulusObject.BatchingVector(
       4096,
       Int32Array.from([20, 20, 20])
     )
     expect(spyOn).toHaveBeenCalledWith(4096, Int32Array.from([20, 20, 20]))
-    expect(vectSmallModulus).toBeDefined()
-    expect(typeof vectSmallModulus.constructor).toBe('function')
-    expect(vectSmallModulus).toBeInstanceOf(Object)
-    expect(vectSmallModulus.constructor.name).toBe('std$$vector$SmallModulus$')
+    expect(vectModulus).toBeDefined()
+    expect(typeof vectModulus.constructor).toBe('function')
+    expect(vectModulus).toBeInstanceOf(Object)
+    expect(vectModulus.constructor.name).toBe('std$$vector$Modulus$')
   })
-  test('It should fail to create a Vector of SmallModulus', () => {
+  test('It should fail to create a Vector of Modulus', () => {
     const spyOn = jest.spyOn(PlainModulusObject, 'BatchingVector')
     expect(() =>
       PlainModulusObject.BatchingVector(4095, Int32Array.from([20, 20, 20]))
