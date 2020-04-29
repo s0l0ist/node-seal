3fa10c83d2c0472570e48e2a1c89f9c1e6b806e9
Nick Angelou <angelou.nick@gmail.com>
2020-04-28 19:31:12 -0500

    wip: coverage tests

--- src/test/unit/coeff-modulus.test.js
+++ src/test/unit/coeff-modulus.test.js
@@ -38,46 +38,46 @@ describe('CoeffModulus', () => {
     expect(spyOn).toHaveBeenCalledWith(4096, Morfix.SecurityLevel.tc256)
     expect(typeof count).toBe('number')
   })
-  test('It should a return a default Vector of SmallModulus with a default security level', () => {
+  test('It should a return a default Vector of Modulus with a default security level', () => {
     const spyOn = jest.spyOn(CoeffModulusObject, 'BFVDefault')
     const vect = CoeffModulusObject.BFVDefault(4096)
     expect(spyOn).toHaveBeenCalledWith(4096)
     expect(vect).toBeDefined()
     expect(typeof vect.constructor).toBe('function')
     expect(vect).toBeInstanceOf(Object)
-    expect(vect.constructor.name).toBe('std$$vector$SmallModulus$')
+    expect(vect.constructor.name).toBe('std$$vector$Modulus$')
   })
-  test('It should a return a default Vector of SmallModulus with a specified security level', () => {
+  test('It should a return a default Vector of Modulus with a specified security level', () => {
     const spyOn = jest.spyOn(CoeffModulusObject, 'BFVDefault')
     const vect = CoeffModulusObject.BFVDefault(4096, Morfix.SecurityLevel.tc256)
     expect(spyOn).toHaveBeenCalledWith(4096, Morfix.SecurityLevel.tc256)
     expect(vect).toBeDefined()
     expect(typeof vect.constructor).toBe('function')
     expect(vect).toBeInstanceOf(Object)
-    expect(vect.constructor.name).toBe('std$$vector$SmallModulus$')
+    expect(vect.constructor.name).toBe('std$$vector$Modulus$')
   })
-  test('It should fail to return a default Vector of SmallModulus', () => {
+  test('It should fail to return a default Vector of Modulus', () => {
     const spyOn = jest.spyOn(CoeffModulusObject, 'BFVDefault')
     expect(() =>
       CoeffModulusObject.BFVDefault(4095, Morfix.SecurityLevel.tc128)
     ).toThrow()
     expect(spyOn).toHaveBeenCalledWith(4095, Morfix.SecurityLevel.tc128)
   })
-  test('It should a create a Vector of SmallModulus', () => {
+  test('It should a create a Vector of Modulus', () => {
     const spyOn = jest.spyOn(CoeffModulusObject, 'Create')
     const vect = CoeffModulusObject.Create(4096, Int32Array.from([36, 36, 37]))
     expect(spyOn).toHaveBeenCalledWith(4096, Int32Array.from([36, 36, 37]))
     expect(vect).toBeDefined()
     expect(typeof vect.constructor).toBe('function')
     expect(vect).toBeInstanceOf(Object)
-    expect(vect.constructor.name).toBe('std$$vector$SmallModulus$')
+    expect(vect.constructor.name).toBe('std$$vector$Modulus$')
   })
-  test('It should fail to create a Vector of SmallModulus', () => {
+  test('It should fail to create a Vector of Modulus', () => {
     const spyOn = jest.spyOn(CoeffModulusObject, 'Create')
     expect(() => CoeffModulusObject.Create(4095, [36, 36, 37])).toThrow()
     expect(spyOn).toHaveBeenCalledWith(4095, [36, 36, 37])
   })
-  test('It should fail to create a Vector of SmallModulus', () => {
+  test('It should fail to create a Vector of Modulus', () => {
     const spyOn = jest.spyOn(CoeffModulusObject, 'Create')
     expect(() =>
       CoeffModulusObject.Create(4095, Int32Array.from([36, 36, 37]))
