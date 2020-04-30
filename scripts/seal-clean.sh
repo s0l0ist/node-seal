5b450956aebffdc9e0db93aa954d007f6e3f7119
Nick Angelou <angelou.nick@gmail.com>
2020-04-30 17:22:06 -0500

    chore: added more cmake paths for cleanup script

--- scripts/seal-clean.sh
+++ scripts/seal-clean.sh
@@ -6,7 +6,14 @@ rm -rf ./bin/*
 rm -rf ./lib/*
 rm -rf ./CMakeFiles
 rm -rf ./CMakeCache.txt
-cd ./native
-rm -rf ./src/CMakeFiles
-rm -rf ./src/CMakeCache.txt
+rm -rf ./native/src/CMakeFiles
+rm -rf ./native/src/CMakeCache.txt
+rm -rf ./thirdparty/zlib/CMakeFiles
+rm -rf ./thirdparty/zlib/CMakeCache.txt
+rm -rf ./thirdparty/zlib/src/CMakeFiles
+rm -rf ./thirdparty/zlib/src/CMakeCache.txt
+rm -rf ./thirdparty/msgsl/CMakeFiles
+rm -rf ./thirdparty/msgsl/CMakeCache.txt
+rm -rf ./thirdparty/msgsl/src/CMakeFiles
+rm -rf ./thirdparty/msgsl/src/CMakeCache.txt
 cd ../../
