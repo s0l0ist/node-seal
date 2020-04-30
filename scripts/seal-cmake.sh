fd96abc498c89690e5416b8d5d4725ff9fce7093
Nick Angelou <nick@morfix.io>
2020-04-30 10:01:07 -0500

    WIP: playing with compiler flags

--- scripts/seal-cmake.sh
+++ scripts/seal-cmake.sh
@@ -4,7 +4,11 @@ source ./submodules/emsdk/emsdk_env.sh > /dev/null
 cd ./submodules/SEAL/
 emcmake cmake \
   -DSEAL_USE_CXX17=ON \
+  -DCMAKE_CXX_FLAGS="-std=c++17 -fomit-frame-pointer" \
   -DCMAKE_CXX_FLAGS_RELEASE="-O3 -DNDEBUG" \
+  -DCMAKE_MODULE_LINKER_FLAGS_RELEASE="-O3" \
+  -DCMAKE_C_FLAGS_RELEASE="-DNDEBUG -O3" \
+  -DCMAKE_EXE_LINKER_FLAGS_RELEASE="-O3" \
   -DSEAL_USE_INTRIN=OFF \
   -DSEAL_USE_ZLIB=ON \
   -DSEAL_USE_MSGSL=ON \
