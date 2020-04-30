55df5c0ceee9a5f5dd14dfa48a93afaca1598d9a
Nick Angelou <angelou.nick@gmail.com>
2020-04-30 10:22:54 -0500

    wip: adding more optimizations

--- scripts/seal-cmake.sh
+++ scripts/seal-cmake.sh
@@ -4,9 +4,10 @@ source ./submodules/emsdk/emsdk_env.sh > /dev/null
 cd ./submodules/SEAL/
 emcmake cmake \
   -DSEAL_USE_CXX17=ON \
-  -DCMAKE_CXX_FLAGS="-std=c++17 -fomit-frame-pointer" \
+  -DCMAKE_CXX_FLAGS="-std=c++17 -O3 -fomit-frame-pointer" \
   -DCMAKE_CXX_FLAGS_RELEASE="-O3 -DNDEBUG" \
   -DCMAKE_MODULE_LINKER_FLAGS_RELEASE="-O3" \
+  -DCMAKE_C_FLAGS="-O3 -fomit-frame-pointer" \
   -DCMAKE_C_FLAGS_RELEASE="-DNDEBUG -O3" \
   -DCMAKE_EXE_LINKER_FLAGS_RELEASE="-O3" \
   -DSEAL_USE_INTRIN=OFF \
