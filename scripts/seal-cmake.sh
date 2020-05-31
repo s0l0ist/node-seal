da8f866dd29bb17f00a45cb24babff9b8cd167cc
Nick Angelou <nick@morfix.io>
2020-05-30 21:52:25 -0500

    wip: waiting for seal issue #179

--- scripts/seal-cmake.sh
+++ scripts/seal-cmake.sh
@@ -4,8 +4,8 @@ source ./submodules/emsdk/emsdk_env.sh > /dev/null
 cd ./submodules/SEAL/
 emcmake cmake \
   -DSEAL_USE_CXX17=ON \
-  -DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG -O3" \
-  -DCMAKE_C_FLAGS_RELEASE="-DNDEBUG -O3" \
+  -DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG -flto -O3" \
+  -DCMAKE_C_FLAGS_RELEASE="-DNDEBUG -flto -O3" \
   -DSEAL_USE_INTRIN=OFF \
   -DSEAL_USE_ZLIB=ON \
   -DSEAL_USE_MSGSL=ON \
