d054cfd89b08825ebeed9084d9cd5332a67443e8
Nick Angelou <nick.angelou@panther.io>
2022-03-02 21:36:08 -0600

    Update to latest seal commits (#124)
    * update deps and add version to runtime
    
    * remove morfix emsdk
    
    * use upstream emsdk
    
    * update submodule scripts
    
    * update to latest seal commits
    
    * add rollup-json plugin

--- scripts/em-update.sh
+++ scripts/em-update.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-# Sets up llvm.
 cd submodules/emsdk/
-git checkout main
-git fetch upstream
-git merge upstream/main --no-edit
 ./emsdk install latest
 ./emsdk activate latest
 cd ../../
